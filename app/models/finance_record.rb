6e2808205ce407be63c3e21edc89dfbe1a6b3867
unknown <pedrohrs08@gmail.com>
2014-01-30 20:03:40 -0200

    adding multiplier

--- app/models/finance_record.rb
+++ app/models/finance_record.rb
@@ -1,5 +1,9 @@
 class FinanceRecord < ActiveRecord::Base
 
+	def multiplier
+		expense? ? -1 : 1
+	end
+
 	scope :from_date, ->(date){ where('date >= ?', date) }
 	scope :to_date, ->(date){ where('date <= ?', date)}
 end
--- test/models/finance_record_test.rb
+++ test/models/finance_record_test.rb
@@ -8,6 +8,7 @@ class FinanceRecordTest < ActiveSupport::TestCase
      record.should respond_to 'date'
      record.should respond_to 'description'
      record.should respond_to 'expense?'
+     record.should respond_to 'multiplier'
    end
 
    test "should respond to scope methods" do
@@ -17,7 +18,7 @@ class FinanceRecordTest < ActiveSupport::TestCase
    
    test "should only return records from a given date" do
       finance_records = FinanceRecord.from_date(Date.new(2000,1,1))
-      finance_records.should have(2).items
+      finance_records.should have(3).items
       finance_records[0].date.should > Date.new(2000-1-1)
    end
 
@@ -27,5 +28,15 @@ class FinanceRecordTest < ActiveSupport::TestCase
       finance_records.each do |finance_record|
       	finance_record.date.should <= Date.new(2000,1,1)
       end
+   end
+
+   test "multiplier should be -1 for expenses" do
+     finance_record = FinanceRecord.find_by_description('Supermarket')
+     finance_record.multiplier.should eq -1
+   end
+
+   test "multiplier should be 1 for incomes" do
+     finance_record = FinanceRecord.find_by_description('Salary')
+     finance_record.multiplier.should eq 1
    end
 end
