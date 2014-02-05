c8564de0821b18798792054190009b9ea5cb63f8
unknown <pedrohrs08@gmail.com>
2014-02-05 15:29:29 -0200

    adding coveralls to the very top

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,9 +2,10 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'coveralls'
+Coveralls.wear!('rails')
+
 require 'rspec/rails'
 require 'rspec/autorun'
-Coveralls.wear!('rails')
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
