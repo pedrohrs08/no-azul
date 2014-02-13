37bfee71b1a7b7eb645622cf6b28b00d4bfa4e73
Pedro <pedrohrs08@gmail.com>
2014-02-13 17:23:10 -0200

    not supporting ruby 1.9.3 anymore | finalizing postgresql conversion

--- app/models/finance_record.rb
+++ app/models/finance_record.rb
@@ -9,10 +9,10 @@ class FinanceRecord < ActiveRecord::Base
 	
     scope :sum_processed, ->{incomes.processed.sum('value') - expenses.processed.sum('value')}
     scope :sum_unprocessed, ->{incomes.unprocessed.sum('value') - expenses.unprocessed.sum('value')}
-	scope :expenses, ->{where('expense == ?', true)}
-	scope :incomes, ->{where('expense == ?',false)}
-	scope :processed, ->{where('processed == ?', true)}
-	scope :unprocessed, ->{where('processed == ?', false)}  
+	scope :expenses, ->{where('expense = ?', true)}
+	scope :incomes, ->{where('expense = ?',false)}
+	scope :processed, ->{where('processed = ?', true)}
+	scope :unprocessed, ->{where('processed = ?', false)}  
 	scope :from_date, ->(date){ where('date >= ?', date)}
 	scope :to_date, ->(date){ where('date <= ?', date)}
 end
